a1c3612327477c16b9415ec1570a6066d5b2dad5
spc-web-dev <itspc2023@gmail.com>
2024-11-27 19:04:16 +0700

    update db.ts

--- lib/drizzle/db.ts
+++ lib/drizzle/db.ts
@@ -6,4 +6,7 @@ import { drizzle } from 'drizzle-orm/vercel-postgres';
 const db = drizzle(sql, { schema })
 
 
-
+export const getUser = async (user: schema.TableUserType)=>{
+    const users = await db.select().from(schema.usersTable)
+    return users
+}
