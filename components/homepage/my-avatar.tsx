ec1ac2315e14d5a8a4457378ab4dfeb3bcc10076
spc-web-dev <itspc2023@gmail.com>
2024-12-09 03:52:30 +0700

    fix home page 2

--- components/homepage/my-avatar.tsx
+++ components/homepage/my-avatar.tsx
@@ -5,7 +5,6 @@ import { motion } from "motion/react";
 const Myavatar = () => {
   return (
     <motion.div
-      className="relative w-[95%] h-[85%] overflow-hidden rounded-3xl"
       initial={{
         opacity: 0,
         scale: 0,
@@ -14,21 +13,23 @@ const Myavatar = () => {
         opacity: 1,
         scale: 1,
         transition: {
-          delay: .2,
+          delay: 0.2,
           duration: 1,
         },
       }}
       viewport={{ once: true }}
     >
-      <Image
-        src={"/images/avatar.jpg"}
-        alt="avarta"
-        width={800}
-        height={800}
-        className=" absolute object-cover w-full h-full"
-        priority
-        quality={95}
-      />
+      <span className="relative w-[95%] h-[85%] overflow-hidden rounded-3xl">
+        <Image
+          src={"/images/avatar.jpg"}
+          alt="avarta"
+          width={800}
+          height={800}
+          className=" absolute object-cover w-full h-full"
+          priority
+          quality={95}
+        />
+      </span>
     </motion.div>
   );
 };
